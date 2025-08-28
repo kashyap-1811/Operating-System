67a44ea3c7aa572c9c42abd2dd9315d755830aec
kashyap-1811 <kashyaprupareliya1811@gmail.com>
2025-08-28 22:25:08 +0530

    PS command updated and robust version

--- LAB06/ps.c
+++ LAB06/ps.c
@@ -21,15 +21,16 @@ int main() {
         // Only consider numeric directories (which are PIDs)
         if (isdigit(dptr->d_name[0])) {
             char path[256];
-            snprintf(path, sizeof(path), "/proc/%s/comm", dptr->d_name);
+            strcpy(path, "/proc/");
+            strcat(path, dptr->d_name);
+            strcat(path, "/stat");
 
             FILE *fp = fopen(path, "r");
             if (fp) {
+                int pid;
                 char cmd[256];
-                if (fgets(cmd, sizeof(cmd), fp) != NULL) {
-                    // remove trailing newline
-                    cmd[strcspn(cmd, "\n")] = 0;
-                    printf("%s\t%s\n", dptr->d_name, cmd);
+                if (fscanf(fp, "%d (%[^)])", &pid, cmd) == 2) {
+                    printf("%d\t%s\n", pid, cmd);
                 }
                 fclose(fp);
             }
